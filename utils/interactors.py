864d4bec1683383bf9cf4812cb3e4d7e2bf59a7e
Nikita Shlapak <n.shapak@mendev.ru>
2024-07-15 14:54:20 +0300

    db added

---
+++ utils/interactors.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import Session
+
+from db.models import User
+
+
+class BaseDBInteractor:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create_user(self, user: User):
+        self.db.add(user)
+        self.db.commit()
+        self.db.refresh(user)
+        return user
+
+    def get_user(self, user_id):
+        return self.db.query(User).filter(User.id == user_id).first()
