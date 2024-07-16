5aa4f37230262849f5203720324ce73b2cf1c387
Nikita <cicikinnikita@gmail.com>
2024-07-16 16:50:03 +0300

    Fixed some bugs

--- utils/Google_sheets.py
+++ utils/Google_sheets.py
@@ -5,14 +5,16 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from env import scopes
+from env import sample_spreadsheet_ID
 
-scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+sample_range_name_of_programming_circle = "Кружок Программирования!A2:B50"
+sample_range_name_of_student_scientific_society = "Студенческое научное общество!A2:B50"
+sample_range_name_of_curators = "Кураторы ИАТЭ НИЯУ МИФИ!A2:B50"
+sample_range_name_of_council_of_dormitories = "Совет общежитий (СО)!A2:B50"
 
-sample_spreadsheet_ID = "1s3WEnxUGNOw9CzJc34ohm6F5KIPswM56ABLbihgPO7s"
-sample_range_name = "Кружок Программирования!A2:B50"
-
-
-def main():
+def reading_google_sheets (sample_range_name):
+  i=0
   creds = None
   if os.path.exists("token.json"):
     creds = Credentials.from_authorized_user_file("token.json", scopes)
@@ -26,40 +28,31 @@ def main():
       creds = flow.run_local_server(port=0)
     with open("token.json", "w") as token:
       token.write(creds.to_json())
+  while i<1:
+    try:
+      service = build("sheets", "v4", credentials=creds)
 
-  try:
-    service = build("sheets", "v4", credentials=creds)
-
-    sheet = service.spreadsheets()
-    result = (
-        sheet.values()
-        .get(spreadsheetId=sample_spreadsheet_ID, range=sample_range_name)
-        .execute()
-    )
-    values = result.get("values", [])
-    question=[]
-    answer=[]
-    if not values:
+      sheet = service.spreadsheets()
+      result = (
+          sheet.values()
+          .get(spreadsheetId=sample_spreadsheet_ID, range=sample_range_name)
+          .execute()
+      )
+    except HttpError as err:
       return
-    for row in values:
-      question.append(row[0])
-      answer.append(row[1])
-      dictionary=dict(zip(question,answer))
+    else:
+      values = result.get("values", None)
+      question=[]
+      answer=[]
+      if values is None:
+        return
+      for row in values:
+        question.append(row[0])
+        answer.append(row[1])
+        dictionary=dict(zip(question,answer))
+      i+=1
     return dictionary
-  except HttpError as err:
-    return
-dictionary_of_programming_circle=main()
-
-sample_range_name = "Студенческое научное общество!A2:B50"
-dictionary_of_student_scientific_society=main()
-
-sample_range_name = "Кураторы ИАТЭ НИЯУ МИФИ!A2:B50"
-dictionary_of_curators=main()
-
-sample_range_name = "Совет общежитий (СО)!A2:B50"
-dictionary_of_council_of_dormitories=main()
-
-print(dictionary_of_programming_circle)
-print(dictionary_of_student_scientific_society)
-print(dictionary_of_curators)
-print(dictionary_of_council_of_dormitories)
+dictionary_of_programming_circle=reading_google_sheets(sample_range_name_of_programming_circle)
+dictionary_of_student_scientific_society=reading_google_sheets(sample_range_name_of_student_scientific_society)
+dictionary_of_curators=reading_google_sheets(sample_range_name_of_curators)
+dictionary_of_council_of_dormitories=reading_google_sheets(sample_range_name_of_council_of_dormitories)
