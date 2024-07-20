966e61d3c3ca30286084ac01ebdae107a07ea3d0
Nikita <cicikinnikita@gmail.com>
2024-07-20 15:08:10 +0300

    Fixed some bugs

--- utils/Google_sheets.py
+++ utils/Google_sheets.py
@@ -8,13 +8,14 @@ from googleapiclient.errors import HttpError
 from env import scopes
 from env import sample_spreadsheet_ID
 
-sample_range_name_of_programming_circle = "Кружок Программирования!A2:B50"
-sample_range_name_of_student_scientific_society = "Студенческое научное общество!A2:B50"
-sample_range_name_of_curators = "Кураторы ИАТЭ НИЯУ МИФИ!A2:B50"
-sample_range_name_of_council_of_dormitories = "Совет общежитий (СО)!A2:B50"
+sample_range_name_of_programming_circle = "Кружок Программирования"
+sample_range_name_of_student_scientific_society = "Студенческое научное общество"
+sample_range_name_of_curators = "Кураторы ИАТЭ НИЯУ МИФИ"
+sample_range_name_of_council_of_dormitories = "Совет общежитий (СО)"
 
-def reading_google_sheets (sample_range_name):
-  i=0
+def reading_google_sheets (name_of_sheet):
+  sample_range_name=name_of_sheet + "!A2:B50"
+  connected=False
   creds = None
   if os.path.exists("token.json"):
     creds = Credentials.from_authorized_user_file("token.json", scopes)
@@ -28,7 +29,7 @@ def reading_google_sheets (sample_range_name):
       creds = flow.run_local_server(port=0)
     with open("token.json", "w") as token:
       token.write(creds.to_json())
-  while i<1:
+  while connected==False:
     try:
       service = build("sheets", "v4", credentials=creds)
 
@@ -39,20 +40,16 @@ def reading_google_sheets (sample_range_name):
           .execute()
       )
     except HttpError as err:
-      return
+      connected=False
     else:
-      values = result.get("values", None)
-      question=[]
-      answer=[]
-      if values is None:
-        return
-      for row in values:
-        question.append(row[0])
-        answer.append(row[1])
-        dictionary=dict(zip(question,answer))
-      i+=1
-    return dictionary
-dictionary_of_programming_circle=reading_google_sheets(sample_range_name_of_programming_circle)
-dictionary_of_student_scientific_society=reading_google_sheets(sample_range_name_of_student_scientific_society)
-dictionary_of_curators=reading_google_sheets(sample_range_name_of_curators)
-dictionary_of_council_of_dormitories=reading_google_sheets(sample_range_name_of_council_of_dormitories)
+      connected=True
+  values = result.get("values", None)
+  question=[]
+  answer=[]
+  if values is None:
+    return
+  for row in values:
+    question.append(row[0])
+    answer.append(row[1])
+    dictionary=dict(zip(question,answer))
+  return dictionary
